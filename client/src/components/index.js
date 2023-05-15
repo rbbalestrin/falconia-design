4c6d74f9bf2114a3b6c5ae863663b8c81e4f14bb
Themir <98977837+themirforget@users.noreply.github.com>
2023-05-15 11:05:19 -0300

    Done the Customizer

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,7 @@
 import CustomButton from "./CustomButton";
-export { CustomButton }
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+import ColorPicker from "./ColorPicker";
+
+export { CustomButton, AIPicker, FilePicker, Tab, ColorPicker };
