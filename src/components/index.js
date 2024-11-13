f35482315f80f4f0f73edea2f58f55bb0b6c03d5
rb.balestrin <therealrenanbez@gmail.com>
2024-11-13 16:35:55 -0300

    mantained

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import CustomButton from "./CustomButton";
-import AIPicker from "./AIPicker";
-import FilePicker from "./FilePicker";
-import Tab from "./Tab";
-import ColorPicker from "./ColorPicker";
-
-export { CustomButton, AIPicker, FilePicker, Tab, ColorPicker };
